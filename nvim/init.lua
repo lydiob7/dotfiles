4aec5e66b0518797918f5d3b4a93fdf84c5f40ac
Tomi Scattini <tomiscattini@Tomis-Mac-mini.local>
2025-11-11 10:20:43 +0100

    Update: use neo-tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,7 +16,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -1239,8 +1239,6 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-
-vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
 vim.o.foldmethod = 'indent'
 vim.o.foldlevel = 99 -- keeps everything open by default
--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -4,30 +4,94 @@
 -- See the kickstart.nvim README for more information
 return {
   {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
     dependencies = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- optional, but recommended
+      'MunifTanjim/nui.nvim',
     },
-    config = function()
-      require('nvim-tree').setup {
-        view = {
-          width = 30,
-          side = 'left',
+    opts = {
+      close_if_last_window = true,
+      popup_border_style = 'rounded',
+      enable_git_status = true,
+      default_component_configs = {
+        icon = {
+          folder_closed = '',
+          folder_open = '',
+          folder_empty = '',
+          default = '',
+          highlight = 'NeoTreeFileIcon',
         },
-        actions = {
-          open_file = {
-            quit_on_open = false,
+        git_status = {
+          symbols = {
+            added = '✚',
+            modified = '',
+            deleted = '✖',
+            renamed = '󰁕',
+            untracked = '',
+            ignored = '',
+            unstaged = '󰄱',
+            staged = '',
+            conflict = '',
           },
         },
-      }
+      },
+      filesystem = {
+        follow_current_file = true,
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = true,
+          hide_gitignored = true,
+        },
+      },
+      window = {
+        position = 'left',
+        width = 30,
+        mappings = {
+          ['<C-b>'] = 'close_window',
+        },
+      },
+    },
 
-      -- optional: auto-open tree when starting Neovim
-      vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+    config = function(_, opts)
+      require('neo-tree').setup(opts)
+
+      vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle filesystem<CR>', {
+        desc = 'Toggle Neo-tree',
+        noremap = true,
+        silent = true,
+      })
+
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'NeotreeBufferEnter',
         callback = function()
-          require('nvim-tree.api').tree.open()
+          -- delay to let Neo-tree finish setup
+          vim.defer_fn(function()
+            for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+              local name = vim.api.nvim_buf_get_name(buf)
+              local modifiable = vim.bo[buf].modifiable
+              local listed = vim.bo[buf].buflisted
+              local filetype = vim.bo[buf].filetype
+
+              if name == '' and modifiable and listed and filetype == '' then
+                vim.api.nvim_buf_delete(buf, { force = true })
+              end
+            end
+          end, 100) -- small delay in ms
         end,
       })
+    end,
+  },
+
+  {
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neo-tree/neo-tree.nvim',
+    },
+    config = function()
+      require('lsp-file-operations').setup()
     end,
   },
 
